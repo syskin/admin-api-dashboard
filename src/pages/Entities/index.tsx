f2361185f577aa050a68f98dc13e5ddacdf33a9d
syskin <mcadu@yahoo.fr>
2021-05-21 20:27:38 +0200

    feat: add cool design for entities list

--- src/pages/Entities/index.tsx
+++ src/pages/Entities/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { entities } from '../../config/entities'
 
+import './Entities.scss'
+
 const Entity: React.FC = () => {
   const routes: any[] = []
   entities.forEach((entity) => {
@@ -15,11 +17,15 @@ const Entity: React.FC = () => {
   return (
     <div>
       <h1>Entities page</h1>
-      <div>
+      <div className="entities-wrapper">
         {routes.map((route, index) => {
           return (
-            <div key={index}>
-              <Link to={route.path}>{route.name}</Link>
+            <div className="entity-wrapper" key={index}>
+              <div>
+                <Link to={route.path}>
+                  <span>{route.name}</span>
+                </Link>
+              </div>
             </div>
           )
         })}

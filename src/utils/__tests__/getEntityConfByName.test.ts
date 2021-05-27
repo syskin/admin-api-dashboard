3df15c8e0a49b00a3f01eebeef637059481ffde1
syskin <mcadu@yahoo.fr>
2021-05-27 22:05:08 +0200

    feat: add tests utils

---
+++ src/utils/__tests__/getEntityConfByName.test.ts
@@ -0,0 +1,16 @@
+import getEntityConfiguration from '../getEntityConfByName'
+import { entities } from '../../config/entities'
+import { Configuration } from '../types/EnitityConfiguration'
+describe('Get entity configuration by its name utils method', () => {
+  it('Test an undefined configruation name', () => {
+    const result = getEntityConfiguration()
+    expect(result).toBe(undefined)
+  })
+
+  it('Test an undefined configruation name', () => {
+    entities.forEach((entity: Configuration) => {
+      const result = getEntityConfiguration(entity.name.toLocaleLowerCase())
+      expect(result).toBe(entity)
+    });
+  })
+})
---
+++ src/utils/__tests__/parseFilterByMethod.test.ts
@@ -0,0 +1,21 @@
+import { parseFilterByMethod } from '../parseFilterByMethod'
+describe('Get filter parsed by HTTP method', () => {
+  it('Test an undefined filter', () => {
+    const result = parseFilterByMethod()
+    expect(result).toBe(undefined)
+  })
+  it('Test defined filter with get method', () => {
+    const result = parseFilterByMethod('get', {name: 'test'})
+    expect(result).toBe("?name=test")
+  })
+  it('Test defined filter with any other HTTP method', () => {
+    const filter = { name: 'test'}
+    const result = parseFilterByMethod('post', filter)
+    expect(result).toBe(filter)
+  })
+  it('Test defined filter with any other HTTP method with emptyFields delete', () => {
+    const filter = { name: 'test', emptyField: undefined}
+    const result = parseFilterByMethod('post', filter, true)
+    expect(result).toBe(filter)
+  })
+})

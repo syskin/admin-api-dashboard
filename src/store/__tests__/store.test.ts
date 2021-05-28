feda5adc5ceb644a1a7343769d3f66c71064724f
syskin <mcadu@yahoo.fr>
2021-05-28 20:22:55 +0200

    feat: test store definition

---
+++ src/store/__tests__/store.test.ts
@@ -0,0 +1,7 @@
+import {store, persistor} from '../'
+describe('Test store definition', () => {
+  it('Store and persistor well defined', () => {
+    expect(store).toBeDefined()
+    expect(persistor).toBeDefined()
+  })
+})
